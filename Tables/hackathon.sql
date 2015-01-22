f06f3410b5763c044b76dd7b8b619b92ac053d97
Melissa Thai <thaimp@rose-hulman.edu>
2015-01-22 16:38:35 -0500

    hackathon table complete (may be mistakes?)

--- Tables/hackathon.sql
+++ Tables/hackathon.sql
@@ -1,10 +1,10 @@
-CREATE TABLE 'Hackathon' (
-	id int NOT NULL AUTO_INCREMENT
-	name
-	logo_url
-	cover_image_url
-	location
-	start_date
-	end_date
-	instruction_path
-)
+CREATE TABLE 'hackathon' (
+	id serial PRIMARY KEY
+	name varchar(255) NOT NULL
+	logo_url varchar(255) NOT NULL 
+	cover_image_url varchar(255) NOT NULL
+	location varchar(255) NOT NULL
+	start_date data NOT NULL
+	end_date data NOT NULL
+	instruction_path varchar(255) NOT NULL
+);

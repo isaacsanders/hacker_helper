81cf1fd3be61f131dd1b9288edd4ef70a70bd671
Dax <dax.earl@gmail.com>
2015-01-28 10:50:51 -1000

    added some website stuff

--- Tables/answer.sql
+++ Tables/answer.sql
@@ -3,7 +3,7 @@ CREATE TABLE answer
     hacker_id INT NOT NULL,
     question_id INT NOT NULL,
     answer VARCHAR NOT NULL,
-    
+
     PRIMARY KEY (hacker_id, question_id),
     FOREIGN KEY (hacker_id) REFERENCES hacker (id),
     FOREIGN KEY (question_id) REFERENCES question (id)

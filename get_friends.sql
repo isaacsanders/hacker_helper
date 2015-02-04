785ac605d1a66ea629e008b03b3d80792c38bcc5
Aaron <aaron.golliver@gmail.com>
2015-02-04 17:36:41 -0500

    get_friends returns email and location of all friends

--- get_friends.sql
+++ get_friends.sql
@@ -1,10 +1,14 @@
-CREATE OR REPLACE FUNCTION public.get_friends(_id INT)
-  RETURNS table(id INT)
-  --RETURNS table(id INT, name VARCHAR(255), location VARCHAR(30))
+CREATE OR REPLACE FUNCTION public.get_friends(_id int)
+  RETURNS table(email varchar(30), location varchar(30))
 AS
-$BODY$
+$$
 begin
-  return QUERY (select second_hacker_id from friendship where first_hacker_id = _id union select first_hacker_id from friendship where second_hacker_id = _id);
+  return QUERY
+  (select hacker.email as email, hacker.location as location from (select second_hacker_id as id from friendship where first_hacker_id = _id
+                                   union
+                                   select first_hacker_id as id from friendship where second_hacker_id = _id) as friend_id, hacker
+  WHERE
+    hacker.id = friend_id.id);
 end
-$BODY$
+$$
 LANGUAGE plpgsql VOLATILE;

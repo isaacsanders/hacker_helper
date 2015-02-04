bed7202172b7114995925c7caf0c9a69791ff51e
Melissa Thai <thaimp@rose-hulman.edu>
2015-02-04 15:19:09 -0500

    working on get_friends

--- get_friends.sql
+++ get_friends.sql
@@ -3,11 +3,11 @@ CREATE OR REPLACE FUNCTION public.get_friends(_id INT)
   --RETURNS table(id INT, name VARCHAR(255), location VARCHAR(30))
 AS
 $BODY$
-  declare
-    retval table(id INT);
-    --retval table(id INT, name VARCHAR(255), location VARCHAR(30));
+declare
+  retval table(id INT);
+  --retval table(id INT, name VARCHAR(255), location VARCHAR(30));
 begin
-  retval := (select second_hacker_id from friendship where first_hacker_id = _id);
+  retval := (select second_hacker_id from friendship where first_hacker_id = _id union select first_hacker_id from friendship where second_hacker_id = _id);
     RETURN retval;
 end
 $BODY$

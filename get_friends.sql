31932503376f8b8d5118053a0dcdcb0d84931627
Aaron <aaron.golliver@gmail.com>
2015-02-04 17:37:52 -0500

    get_friends returns id, email, and location of all friends

--- get_friends.sql
+++ get_friends.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE FUNCTION public.get_friends(_id int)
-  RETURNS table(email varchar(30), location varchar(30))
+  RETURNS table(id int, email varchar(30), location varchar(30))
 AS
 $$
 begin
   return QUERY
-  (select hacker.email as email, hacker.location as location from (select second_hacker_id as id from friendship where first_hacker_id = _id
+  (select hacker.id as id, hacker.email as email, hacker.location as location from (select second_hacker_id as id from friendship where first_hacker_id = _id
                                    union
                                    select first_hacker_id as id from friendship where second_hacker_id = _id) as friend_id, hacker
   WHERE

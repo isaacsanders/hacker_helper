fbd6b54ff468717c77bbba9698acb198e457710f
Aaron <aaron.golliver@gmail.com>
2015-02-05 15:21:24 -0500

    Fixed get_my_location

--- SQL Stuff/Stored Procedures/get_my_location.sql
+++ SQL Stuff/Stored Procedures/get_my_location.sql
@@ -1,8 +1,9 @@
-DROP FUNCTION get_my_location(INTEGER)
-CREATE OR REPLACE FUNCTION public.get_my_location(_id int4)
+DROP FUNCTION get_my_location(INTEGER);
+
+CREATE OR REPLACE FUNCTION public.get_my_location(_hacker_id int4)
   RETURNS table(
-  loc_id varchar(30),
-  state varchar(30)
+  loc_id int
+  , state varchar(30)
   , city varchar(30)
   , zipcode varchar(10)
   , country varchar(30)
@@ -12,8 +13,9 @@ AS
 $BODY$
   begin
 return QUERY
-    (Select * from locations where location_id=_id);
+    (Select L.location_id, L.state, L.city, L.zipcode, L.country, L.street_number, L.route from locations as L, hacker where hacker.id=_hacker_id and L.location_id = hacker.location);
 end
 $BODY$
 LANGUAGE plpgsql VOLATILE;
 
+select * from get_my_location(2);

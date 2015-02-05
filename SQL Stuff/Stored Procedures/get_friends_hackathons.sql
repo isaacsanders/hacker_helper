8543bcec3f13fd8c4bab201c464ab2651bea59e9
Aaron <aaron.golliver@gmail.com>
2015-02-05 15:14:20 -0500

    fixed get_friends_hackathons() again...

--- SQL Stuff/Stored Procedures/get_friends_hackathons.sql
+++ SQL Stuff/Stored Procedures/get_friends_hackathons.sql
@@ -1,13 +1,14 @@
+drop function get_friends_hackathons(int);
 CREATE OR REPLACE FUNCTION public.get_friends_hackathons(_id int)
   RETURNS table(friend_id int, hackathon_id int)
 AS
 $$
 begin
   return QUERY
-  (select A.hacker_id as hackathon_id, H.id as friend_id
-   FROM hackathon H, hackathons_user_is_attending A
-   WHERE H.id = A.hackathon_id
-         and exists(select id from get_friends(_id) where A.hacker_id = id));
+  (select hackathons_user_is_attending.hacker_id, hackathon.id
+   FROM hackathon, hackathons_user_is_attending
+   WHERE hackathon.id = hackathons_user_is_attending.hackathon_id
+         and exists(select id from get_friends(_id) where hackathons_user_is_attending.hacker_id = id));
 end
 $$
 LANGUAGE plpgsql VOLATILE;

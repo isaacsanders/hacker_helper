1290d2add5d52380d0e3800bd9fb8940994eb17e
Aaron <aaron.golliver@gmail.com>
2015-02-05 15:35:50 -0500

    Formatted and added testing code

--- SQL Stuff/Stored Procedures/get_hacker_from_oauth.sql
+++ SQL Stuff/Stored Procedures/get_hacker_from_oauth.sql
@@ -1,12 +1,24 @@
-drop function get_hacker_from_oauth(varchar(100));
-CREATE OR REPLACE FUNCTION public.get_hacker_from_oauth(_oauth varchar(100))
-  RETURNS table(id int, email varchar(30), location int, fb_oauth_access_token varchar(100), name varchar(255))
+DROP FUNCTION get_hacker_from_oauth( VARCHAR(100) );
+
+CREATE OR REPLACE FUNCTION public.get_hacker_from_oauth(_oauth VARCHAR(100))
+  RETURNS TABLE(id INT, email VARCHAR(30), location INT, fb_oauth_access_token VARCHAR(100), name VARCHAR(255))
 AS
-$$
-begin
-  return QUERY
-  (select H.id, H.email, H.location, H.fb_oauth_access_token, H.name from hacker as H where H.fb_oauth_access_token = _oauth);
-end
-$$
+  $$
+  BEGIN
+    RETURN QUERY
+    (SELECT
+       H.id,
+       H.email,
+       H.location,
+       H.fb_oauth_access_token,
+       H.name
+     FROM hacker AS H
+     WHERE H.fb_oauth_access_token = _oauth);
+  END
+  $$
 LANGUAGE plpgsql VOLATILE;
 
+
+-- testing code
+SELECT *
+FROM get_hacker_from_oauth('638364292977015');

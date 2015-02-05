6b902c3c6e6ed02e5f95f9073f8d789ed2398e69
Dax <dax.earl@gmail.com>
2015-02-05 10:55:25 -1000

    Get user database call.

--- website/db.py
+++ website/db.py
@@ -4,7 +4,14 @@ conn = psycopg2.connect(database="hacker", user="dax", password="daxiscool")
 
 def get_hacker(id):
     cur = conn.cursor()
-    cur.callproc("get_hacker", id)
+    cur.callproc("get_hacker", (id,))
     user = cur.fetchone()
-    print
-    return user
+    if user is not None:
+        name, fb_token = user
+        cur.close()
+        return {
+            "name": name,
+            "fb_oauth_access_token": fb_token
+        }
+    else:
+        return None

982861e4748ae0dd335ffcfe2ab4b2dfc39e49a5
Dax <dax.earl@gmail.com>
2015-02-05 03:08:30 -1000

    Fixed the database updated the tables

--- SQL Stuff/Stored Procedures/add_location_hacker.sql
+++ SQL Stuff/Stored Procedures/add_location_hacker.sql
@@ -1,13 +1,14 @@
-CREATE OR REPLACE FUNCTION public.add_location_hacker(_street_address varchar, _state varchar, _city varchar, _zipcode int, _country varchar)
+CREATE OR REPLACE FUNCTION public.add_location_hacker(_hacker_id int,_street_address varchar, _state varchar, _city varchar, _zipcode int, _country varchar)
   RETURNS int4
 AS
 $BODY$
   declare
-  iid integer;
+  iid int;
 begin
-  INSERT INTO public.location (street_address, state, city, zipcode,country)
+  INSERT INTO public.locations (street_address, state, city, zipcode,country)
   VALUES (_street_address, _state, _city, _zipcode,_country)
   RETURNING location_id INTO iid;
+
   return iid;
 end;
 $BODY$

3e62814e42a7fa4d7c81ba011cbe9582fda608e3
Isaac Sanders <isaac@isaacbfsanders.com>
2015-02-11 03:32:28 -0500

    Add team creation and organize
    The templates are named inconsistently and needed to be
    organized better.
    
    I am starting there, and may continue to organize the code.

--- SQL Stuff/Stored Procedures/add_team.sql
+++ SQL Stuff/Stored Procedures/add_team.sql
@@ -2,18 +2,17 @@ CREATE OR REPLACE FUNCTION add_team( _creator_id INTEGER, _name VARCHAR(255) )
   RETURNS INTEGER AS
   $$
   DECLARE
-    lid INTEGER;
+    _team_id INTEGER;
   BEGIN
     START TRANSACTION;
 
     INSERT INTO team (name) VALUES (_name)
-    RETURNING location_id
-      INTO lid;
+    RETURNING _team_id;
 
-    INSERT INTO teams_hacker_is_member_of (hacker_id, team_id) VALUES (_creator_id, lid);
+    INSERT INTO teams_hacker_is_member_of (hacker_id, team_id) VALUES (_creator_id, _team_id);
 
     COMMIT TRANSACTION;
-    RETURN 0;
+    RETURN _team_id;
   END
   $$
 LANGUAGE plpgsql VOLATILE;

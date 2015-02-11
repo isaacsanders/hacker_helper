0e2fc733640362f0ab9cfbe0c3edb84f9f6d9d0e
Aaron <aaron.golliver@gmail.com>
2015-02-10 19:58:25 -0500

    Added checks to make sure you cannot add the same hackathon for a user twice

--- SQL Stuff/Stored Procedures/register_user_for_hackathon.sql
+++ SQL Stuff/Stored Procedures/register_user_for_hackathon.sql
@@ -1,10 +1,21 @@
 CREATE OR REPLACE FUNCTION public.register_user_for_hackathon( _hackathon_id INT, _hacker_id INT )
-  RETURNS VOID
+  RETURNS INT
 AS
   $BODY$
   BEGIN
+    START TRANSACTION;
+    IF EXISTS (SELECT *
+               FROM hackathons_user_is_attending H
+               WHERE H.hackathon_id = _hackathon_id AND H.hacker_id = _hacker_id)
+    THEN
+      RETURN 1;
+    END IF;
+
     INSERT INTO hackathons_user_is_attending (hackathon_id, hacker_id)
     VALUES (_hackathon_id, _hacker_id);
+
+    RETURN 0;
+    COMMIT;
   END
   $BODY$
 LANGUAGE plpgsql VOLATILE;
